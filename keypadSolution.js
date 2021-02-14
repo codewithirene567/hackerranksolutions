7536156a6375c599da3ad15b53a47ad5b9170300
codewithirene567 <irenejinscott@gmail.com>
2021-02-14 16:08:30 -0500

    added last of clarifying comments

--- keypadSolution.js
+++ keypadSolution.js
@@ -13,21 +13,30 @@ function numKeypadSolutions(wordlist, keypads) {
         }
       
       let array = []
-    
+    //looking through each of the english words
       for (let i = 0; i<wordlist.length; i++){
         let word = wordlist[i]
         let flag = true
+    //looking through each of the combinations
         for (let j = 0; j<keypads.length; j++){
             let keypad = keypads[j]
+    //checking if the english word includes the first letter of the combination
             if (word.includes(keypad[0])){
+        //if it does, then look through the word itself
                 for (let k = 0; k<word.length; k++){
                     let letter = word[k]
+    //includes returns true or false if a string includes a substring
+    //we are checking if the combination does not include the sought after letter
+    //if it doesn't we need to break out of the loop for this instance and set the flag to false
                     if (keypad.includes(letter) == false){
                     flag = false
                     break;
                     } 
                 }
             }
+    //if the flag is set to true and if the first letter in the combination is included in the word
+    //then we need to count it as an occurance of the word within the combination
+    //then we need to reset the flag to true
           if (flag == true && word.includes(keypad[0])){
                   //console.log("contains")
                   output[keypad] = output[keypad] + 1 
@@ -35,7 +44,8 @@ function numKeypadSolutions(wordlist, keypads) {
               flag = true
             }
           }   
-         
+        //finally return the array which has the values from that object to represent the occurances
+        //of the words being made from the combinations 
            for (let key in output){
             array.push(output[key])
           }
